3b5c3c5f2fb2635af86850cdbe286b4fcb610145
davemarch <davemarchaudio@gmail.com>
2019-03-15 10:32:07 +0000

    added mapping function

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -5,13 +5,14 @@ import NewsItem from '/Users/codenation20/Desktop/GitHub/bbc/src/components/news
 
 
 const Container = (props) => {
+
+    const newsItem = props.news.map((newsInfo) => <NewsItem news={newsInfo} /> );
+
         return (
             <div className='container'>
                 <Header header = {props.header}/>
                 <div className='newsitemdiv'>
-                <NewsItem img={props.news[0].img} headline={props.news[0].headline} subcat={props.news[0].subcat}/>
-                <NewsItem img={props.news[1].img} headline={props.news[1].headline} subcat={props.news[1].subcat}/>
-                <NewsItem img={props.news[2].img} headline={props.news[2].headline} subcat={props.news[2].subcat}/>                
+                {newsItem}
                 </div>
             </div>
         );
--- src/components/newsItem/newsItem.js
+++ src/components/newsItem/newsItem.js
@@ -6,10 +6,10 @@ import img from './imgPlaceholder.jpg'
 const NewsItem = (props) => {
         return (
             <div className='newsItem'>
-            <img className= 'img' src={props.img}/>
+            <img className= 'img' src={props.news.img}/>
             <div className= 'NewsItemText'>
-            <div className='headline'>{props.headline}</div>
-            <div className='category'>{props.subcat}</div>
+            <div className='headline'>{props.news.headline}</div>
+            <div className='category'>{props.news.subcat}</div>
             </div>
             </div>
         );
